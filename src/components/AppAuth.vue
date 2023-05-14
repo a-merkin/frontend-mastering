ad0530e34631dd14bdcadc6c48df01407d2262d5
Арсен Меркин <arsen.merkin@gmail.com>
2023-05-14 22:50:12 +0500

    added auth validator

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <el-form class="auth__form" label-position="top">
-      <el-form-item label="Эл. почта">
+    <el-form
+      ref="ruleFormRef"
+      class="auth__form"
+      label-position="top"
+      :model="form"
+      :rules="rules"
+    >
+      <el-form-item label="Эл. почта" prop="email">
         <el-input v-model="form.email"></el-input>
       </el-form-item>
-      <el-form-item label="Пароль">
+      <el-form-item label="Пароль" prop="password">
         <el-input v-model="form.password" type="password"
           ><template #suffix>
             <el-button link
@@ -12,7 +18,7 @@
         ></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="handleAuth">Войти</el-button>
+        <el-button type="primary" @click="submitForm(ruleFormRef)">Войти</el-button>
         <el-button @click="handleRegistration">Регистрация</el-button>
       </el-form-item>
     </el-form>
@@ -21,21 +27,55 @@
 
 <script setup lang="ts">
 import router from "@/router"
-import { ref } from "vue"
+import { ref, reactive } from "vue"
+import type { FormInstance, FormRules } from "element-plus"
 import { useAppStore } from "@/store/"
 
 const { handleLogin } = useAppStore()
 
-const form = ref(
+const ruleFormRef = ref<FormInstance>()
+
+const form = reactive(
   {} as {
     email: string
     password: string
   }
 )
 
-const handleAuth = () => {
-  handleLogin(form.value).then(() => router.push("/workplace"))
+const validateEmail = (rule: any, value: any, callback: any) => {
+  if (!value) {
+    callback(new Error("Введите email"))
+  } else {
+    callback()
+  }
 }
+
+const validatePassword = (rule: any, value: any, callback: any) => {
+  if (!value) {
+    callback(new Error("Введите пароль"))
+  } else {
+    callback()
+  }
+}
+
+const rules = reactive<FormRules>({
+  password: [{ required: true, validator: validatePassword, trigger: "blur" }],
+  email: [{ required: true, validator: validateEmail, trigger: "blur" }]
+})
+
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.validate((valid: boolean) => {
+    if (valid) {
+      console.log("submit!")
+      handleLogin(form).then(() => router.push("/workplace"))
+    } else {
+      console.log("error submit!")
+      return false
+    }
+  })
+}
+
 const handleRegistration = () => {
   router.push("/register")
 }
