c8750804095e104769bfc05759ee6f54d8f69d56
Арсен Меркин <arsen.merkin@gmail.com>
2023-05-31 22:31:28 +0500

    change dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 import { fileURLToPath } from "url"
 
 export default defineConfig({
+  base: "/frontend-mastering/",
   plugins: [
     vue({
       reactivityTransform: true
