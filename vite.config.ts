ad247354e43d39116592b8c3907d18866c362073
Арсен Меркин <arsen.merkin@gmail.com>
2023-05-31 13:27:51 +0500

    remark vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,56 @@
-import { fileURLToPath, URL } from "node:url"
+import vue from "@vitejs/plugin-vue"
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite"
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+import { fileURLToPath } from "url"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      reactivityTransform: true
+    }),
     AutoImport({
+      imports: [
+        "vue",
+        "vue-router",
+        "@vueuse/head",
+        "pinia",
+        {
+          "@/store": ["useStore"]
+        }
+      ],
+      dts: "src/auto-imports.d.ts",
+      eslintrc: {
+        enabled: true
+      },
       resolvers: [ElementPlusResolver()]
     }),
     Components({
+      dirs: ["src/components"],
+      extensions: ["vue"],
       resolvers: [ElementPlusResolver()]
     })
   ],
   resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
-    }
+    alias: [
+      {
+        find: /@\/components\/((?!.*[.](ts|js|tsx|jsx|vue)$).*$)/,
+        replacement: fileURLToPath(
+          new URL("./src/components/$1/index.vue", import.meta.url)
+        )
+      },
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url))
+      },
+      {
+        find: /@\/services\/((?!.*[.](ts|js|tsx|jsx|vue)$).*$)/,
+        replacement: fileURLToPath(new URL("./src/services/$1/index.ts", import.meta.url))
+      }
+    ]
+  },
+  server: {
+    port: 8080
   }
 })
